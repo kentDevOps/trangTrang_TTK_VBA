0844d427ffca68b1060578131c3c63006466cd65
kentDevOps <data.quaza.3@gmail.com>
2024-09-20 17:23:42 +0700

    Them File gop.py

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# Test Python
+print("Có thể gọi code Python Từ VBA")
+print("Sub RunPythonExe()")
+print("   Dim exePath As String")
+print("   exePath = YourPath")
+print("   Shell exePath, vbNormalFocus")
+print("End Sub")
